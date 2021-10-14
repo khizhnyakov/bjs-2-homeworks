be2232737fc6aba0425d1ce553fada46d90672a8
Pawel Khizhnyakov <pawel.khizhnyakov@gmail.com>
2021-10-14 19:14:08 +0300

    task 1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,24 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let x = [];
+  let d = (b ** 2 - 4 * a * c);
+  if (d === 0) {
+    x[0] = -b / 2 * a;
+  }
+  else if (d > 0) {
+    x[0] = (- b + Math.sqrt(d)) / (2 * a);
+    x[1] = (- b - Math.sqrt(d)) / (2 * a);
+  }
+  return x;
 }
 
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  let totalAmount = [];
+  let S = amount;
+  let P = percent/12;
+  let n = 
+  let payMonth = S * (P + P / (((1 + P) ^ n) - 1));
   return totalAmount;
 }
+
